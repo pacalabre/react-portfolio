cf7a229dfee4b4d815f9cf8d3cc2d988eaa1b944
Paul Calabrese <pacalabre@gmail.com>
2019-03-10 16:34:25 -0700

    added starting page components and page routing

---
+++ src/components/pageComponents/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class Home extends React.Component {
+    render() {
+        return (
+            <p>This is the home page!</p>
+        )
+    }
+}
+
+export default Home;
---
+++ src/components/pageComponents/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+class NotFound extends React.Component {
+    render() {
+        return (
+            <p>This is the NOT FOUND page!</p>
+        )
+    }
+}
+
+export default NotFound;
