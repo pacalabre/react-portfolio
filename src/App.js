f3fcffd4a36f3519c69be4af36ac4325b4115fc5
Paul Calabrese <pacalabre@gmail.com>
2019-03-10 18:09:28 -0700

    added proper Wordpress API end point

--- src/App.js
+++ src/App.js
@@ -4,13 +4,39 @@ import './App.css';
 import Home from './components/pageComponents/Home';
 import IndividualProject from './components/pageComponents/individualProject';
 import NotFound from './components/pageComponents/NotFound';
+import axios from 'axios';
 
 class App extends Component {
+  state = {
+    posts:{}
+  }
+
+  getPosts = post => {
+    var self = this;
+    axios.get('https://public-api.wordpress.com/wp/v2/sites/paulcalabrese.wordpress.com/posts?_embed', {
+      params: {
+          per_page: '100'
+      }
+    })
+    .then(function(response) {
+      self.setState({
+        posts: response.data
+      })
+    })
+    .catch(function(err) {
+      console.log(err);
+    })
+  };
+
+  componentDidMount() {
+    this.getPosts();
+  }
+
   render() {
     return (
       <Switch>
         <Route path="/:project" render={()=> <IndividualProject />} /> 
-        <Route path="/" render = {()=>  <Home /> } />
+        <Route path="/" render = {()=>  <Home posts={ this.state.posts } /> } />
         <Route component = { NotFound } />
       </Switch>
     )
--- src/components/pageComponents/Home.js
+++ src/components/pageComponents/Home.js
@@ -4,7 +4,16 @@ import React from 'react';
 class Home extends React.Component {
     render() {
         return (
-            <p>This is the home page!</p>
+            <div>
+                <h2>This is the home page!</h2>
+
+                <p>Projects</p>
+                {
+                    Object.keys(this.props.posts).map(key => 
+                        <p key={ key }>{ this.props.posts[key].title.rendered }</p>    
+                    )
+                }
+            </div>
         )
     }
 }
