f54f8a0123688877892ba777a4a2455800ae966d
Paul Calabrese <pacalabre@gmail.com>
2019-05-21 20:41:47 -0700

    added breadcrumbs to project pages, also made some tweaks to the layout to adjust the new space taken up by the breadcrumbs

--- src/components/pageComponents/individualProject.js
+++ src/components/pageComponents/individualProject.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import '../../css/individual-project.css';
 import NotFound from './NotFound';
 
 
 class IndividualProject extends React.Component {
-    constructor(props) {
-        // possibly temp creation of a ref, looking for a way to dynamically style and label links
-        super(props);
-        this.description = React.createRef();
-
-        const node =  this.description;
-
-      }
 
     render() {
         if(!this.props.location.state) {
@@ -20,13 +12,18 @@ class IndividualProject extends React.Component {
         }
         return (
             <main>
-                <section className="project-img-and-title-section">
-                    <img className="project-img" src={this.props.location.state.post.jetpack_featured_media_url }/>
-                    <p ref={this.description} dangerouslySetInnerHTML={{ __html: this.props.location.state.post.excerpt.rendered }}></p>
-                </section>
-                <section className="project-img-section">
-                    <p dangerouslySetInnerHTML={{ __html: this.props.location.state.post.content.rendered }}></p>
-                </section>
+                <div className="breadcrumbs">
+                <Link to="/">home</Link> / <Link to="/:projectID">work</Link> / <span>{this.props.location.state.post.title.rendered}</span>
+                </div>
+                <div className="project-post-content">
+                    <section className="project-img-and-title-section">
+                        <img className="project-img" src={this.props.location.state.post.jetpack_featured_media_url }/>
+                        <p dangerouslySetInnerHTML={{ __html: this.props.location.state.post.excerpt.rendered }}></p>
+                    </section>
+                    <section className="project-img-section">
+                        <p dangerouslySetInnerHTML={{ __html: this.props.location.state.post.content.rendered }}></p>
+                    </section>
+                </div>
             </main>
         )
     }
