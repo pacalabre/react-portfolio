695ddf3f28aadb7e56a26579b58feb40b467f99d
Paul Calabrese <pacalabre@gmail.com>
2019-06-25 16:48:54 -0700

    added a new background image to the technology section and renamed some images to be more descriptive.

--- src/components/pageComponents/NotFound.js
+++ src/components/pageComponents/NotFound.js
@@ -8,7 +8,7 @@ class NotFound extends React.Component {
             <div>
                 <p className="four-oh-four-err">404 - Page not found!</p>
                 <p className="four-oh-four-text">But if you want to hear a hip hop beat that I made a couple of years ago, check it out below!</p>
-                <iframe class="iframe-404" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/98030701&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+                <iframe class="iframe-404" scrolling="no" frameborder="no" allow="autoplay" title="A hip hop beat I made" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/98030701&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
             </div>
         )
     }
