bb88807414d11527ada32f6579fd5d633b39f736
Paul Calabrese <pacalabre@gmail.com>
2019-06-25 17:24:24 -0700

    added css to show the video banner in mobile

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../css/about.css';
 import aboutVid from '../assets/about.mp4';
+import aboutVidPoster from '../assets/sea.jpg';
 
 class About extends React.Component {
     render() {
@@ -13,7 +14,7 @@ class About extends React.Component {
                             <p>Driven</p>
                             <span>Development</span>
                         </div>
-                        <video muted autoPlay loop playsinline className="about-video">
+                        <video muted autoPlay loop playsinline poster={aboutVidPoster} className="about-video">
                             <source src={ aboutVid } type="video/mp4" />
                             <source src={ aboutVid } type="video/ogg" />
                             Your browser does not support the video tag.
