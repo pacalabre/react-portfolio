e61be14f7c4636673b064260acabbea04f40a940
Paul Calabrese <pacalabre@gmail.com>
2019-07-02 22:51:47 -0700

    made mtn lake image rotate

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import '../css/about.css';
 import aboutVid from '../assets/about.mp4';
 import aboutVidPoster from '../assets/sea.jpg';
+import mtns from '../assets/mtns-lake-transparent.png';
 
 class About extends React.Component {
     render() {
@@ -9,16 +10,12 @@ class About extends React.Component {
             <section className="about-container">
                 <div className="about">
                     <div className="about-p-background">
+                    <img className="mtns-lake-img" src={mtns}/>
                         <div className="about-p">
                             <p>Design</p>
                             <p>Driven</p>
                             <span>Development</span>
                         </div>
-                        {/* <video muted autoPlay loop playsInline className="about-video">
-                            <source src={ aboutVid } type="video/mp4" />
-                            <source src={ aboutVid } type="video/ogg" />
-                            Your browser does not support the video tag.
-                        </video> */}
                     </div>
                 </div>
                 <div className="headshot"></div>
