310afb23381a61650a1e66a06fd2611086596a34
Paul <pacalabre@gmail.com>
2020-12-31 15:05:17 -0800

    Fixed nav issue where the mobile menu wouldn't close when clicked.

--- src/components/HomeNavigation.js
+++ src/components/HomeNavigation.js
@@ -9,7 +9,7 @@ class HomeNavigation extends React.Component {
 
     getNavbarToggleIcon() {
         return (
-            <div className="toggle-icon-wrapper">
+            <div className="toggle-icon-wrapper home-nav-toggler">
                 <span className="toggle-icon-line toggle-line-top"></span>
                 <span className="toggle-icon-line toggle-line-middle"></span>
                 <span className="toggle-icon-line toggle-line-bottom"></span>
@@ -19,26 +19,26 @@ class HomeNavigation extends React.Component {
     
     render() {
         return (
-            <Navbar className="home-navigation" expand="lg">
-                <div className="home-navigation-text">
-                    <Link className="home-nav-link-home"  to="/">
-                        <img className="home-nav-logo" alt="logo" src={logo} />
-                    </Link>
-                    <Navbar.Toggle children={this.getNavbarToggleIcon()}  aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="home-navbar-nav">
-                        <div className="home-nav-link-container">
-                            <Link className="home-nav-link-to link-to" to="/about">about</Link>
-                            <Link className="home-nav-link-to link-to" to="/#work">work</Link>
-                            <Link className="home-nav-link-to link-to" to="/#contact">contact</Link>
-                        </div> 
+            <Navbar collapseOnSelect expand="lg" className="home-navigation">
+                <Navbar.Brand href="#home" className="home-navigation-text">
+                <Link className="home-nav-link-home"  to="/">
+                    <img className="home-nav-logo" alt="logo" src={logo} />
+                </Link>
+                </Navbar.Brand>
+                <Navbar.Toggle children={this.getNavbarToggleIcon()} aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="home-navbar-nav">
+                        <div class="home-navbar-link-container">
+                            <Link className="home-nav-link-to link-to" to="/about">ABOUT</Link>
+                            <Nav.Link className="home-nav-link-to link-to" href="/#work">WORK</Nav.Link>
+                            <Nav.Link className="home-nav-link-to link-to" href="/#contact">CONTACT</Nav.Link>
+                        </div>
                         <div className="home-nav-social-icons">
                             <a className="social-icons-a" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/pacalabre/"><span className="icon-social-linkedin nav-ul-social"></span></a>
                             <a className="social-icons-a" target="_blank" rel="noopener noreferrer" href="https://github.com/pacalabre"><span className="icon-social-github nav-ul-social"></span></a>
                         </div>
-                        </Nav>
-                    </Navbar.Collapse>
-                </div>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
         )
     }
