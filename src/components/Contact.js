58785491dfeb489a4b55df425c10eabbb741e52f
Paul <pacalabre@gmail.com>
2021-03-02 13:06:00 -0800

    Added contact form redesign styles.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,10 +8,13 @@ const Contact = () => {
             <section className="contact-section" id="contact">
                 <h2>Contact</h2>
                 <form  method="POST" action="https://formspree.io/pacalabre@gmail.com">
-                    <input type="name" name="name" placeholder="Name"/>
-                    <input type="email" name="email" placeholder="Email"/>
-                    <textarea name="message" placeholder="Message"></textarea>
-                    <div>
+                    <label className="form-label">name</label>
+                    <input type="name" name="name"/>
+                    <label className="form-label">email</label>
+                    <input type="email" name="email"/>
+                    <label className="form-label">message</label>
+                    <textarea name="message"></textarea>
+                    <div className="contact-button-container">
                         <input type="hidden" name="_next" value="portfolio-form-submit.html" />
                         <button className="submit-button" type="submit">Send</button>
                     </div>
