3412c523a4e6f344cd2edb554f36d326e8dec362
Paul <pacalabre@gmail.com>
2021-07-09 15:08:49 -0700

    Added for attribute to labels for accessibility.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,12 +8,12 @@ const Contact = () => {
             <section className="contact-section" id="contact">
                 <h2>Contact</h2>
                 <form  method="POST" action="https://formspree.io/pacalabre@gmail.com">
-                    <label className="form-label">name</label>
-                    <input type="name" name="name"/>
-                    <label className="form-label">email</label>
-                    <input type="email" name="email"/>
-                    <label className="form-label">message</label>
-                    <textarea name="message"></textarea>
+                    <label for="name" className="form-label">name</label>
+                    <input id="name" type="name" name="name"/>
+                    <label for="email" className="form-label">email</label>
+                    <input id="email" type="email" name="email"/>
+                    <label for="message" className="form-label">message</label>
+                    <textarea id="message" name="message"></textarea>
                     <div className="contact-button-container">
                         <input type="hidden" name="_next" value="portfolio-form-submit.html" />
                         <button className="submit-button" type="submit">Send</button>
